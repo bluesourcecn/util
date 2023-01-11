e001a8c6fca13b0ed08dfc36669d3353e9eeec24
bluesource <37376468+bluesourcecn@users.noreply.github.com>
2023-01-11 17:17:07 +0800

    add content for ConsoleHandler

--- src/com/cn/config/ConsoleHandler.java
+++ src/com/cn/config/ConsoleHandler.java
@@ -1,1 +1,19 @@
+package com.cn.config;
 
+import java.util.logging.LogRecord;
+
+public class ConsoleHandler extends java.util.logging.ConsoleHandler {
+	public ConsoleHandler() {
+		super();
+		setOutputStream(System.out);
+	}
+	@Override
+	public void publish(LogRecord record) {
+		super.publish(record);
+	}
+	@Override
+    public void close() {
+        flush();
+    }
+	
+}
